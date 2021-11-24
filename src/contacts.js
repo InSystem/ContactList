bca80977324b0045a05360c02350d723b72e2458
hr21don <h_R@helitharupasinghe.com>
2021-11-24 16:14:33 +0000

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    img:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    tel: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    name: "Johnny Depp",
+    img:
+      "https://m.media-amazon.com/images/M/MV5BMTM0ODU5Nzk2OV5BMl5BanBnXkFtZTcwMzI2ODgyNQ@@._V1_UY317_CR4,0,214,317_AL_.jpg",
+    tel: "+987 654 321",
+    email: "johnny@Deep.com"
+  },
+  {
+    name: "Chuck Norris",
+    img:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    tel: "+918 372 574",
+    email: "gmail@chuckmail.com"
+  }
+];
+export default contacts;
